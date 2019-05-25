b28b9b30c4f0185c2c3a61b28d59f5bd32b971a1
Will Usher <willusher.life@gmail.com>
2019-05-25 08:41:14 -0600

    remove comment

--- main.cpp
+++ main.cpp
@@ -234,15 +234,6 @@ void run_app(int argc, const char **argv, SDL_Window *window) {
 	bool camera_changed = true;
 	bool material_changed = true;
 	while (!done) {
-		// Poll and handle events (inputs, window resize, etc.)
-		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
-		// tell if dear imgui wants to use your inputs.
-		// - When io.WantCaptureMouse is true, do not dispatch mouse input data
-		//     to your main application.
-		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data
-		//     to your main application.
-		// Generally you may always pass all inputs to dear imgui, and hide them from your
-		// application based on those two flags.
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
 			ImGui_ImplSDL2_ProcessEvent(&event);
