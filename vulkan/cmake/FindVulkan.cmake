4b80c4412dd97004ce69df04e50db07539076b66
Will Usher <will@sci.utah.edu>
2019-08-31 11:04:46 -0600

    fix weird indent

--- vulkan/cmake/FindVulkan.cmake
+++ vulkan/cmake/FindVulkan.cmake
@@ -29,98 +29,98 @@
 #
 
 if(WIN32)
-	find_path(Vulkan_INCLUDE_DIR
-		NAMES vulkan/vulkan.h
-		PATHS
-		${VULKAN_SDK}/Include
-		$ENV{VULKAN_SDK}/Include
-	)
+    find_path(Vulkan_INCLUDE_DIR
+        NAMES vulkan/vulkan.h
+        PATHS
+        ${VULKAN_SDK}/Include
+        $ENV{VULKAN_SDK}/Include
+    )
 
-	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-		find_library(Vulkan_LIBRARY
-			NAMES vulkan-1
-			PATHS
-			${VULKAN_SDK}/Lib
-			${VULKAN_SDK}/Bin
-			$ENV{VULKAN_SDK}/Lib
-			$ENV{VULKAN_SDK}/Bin
-		)
-		find_program(SPIRV_COMPILER
-			NAMES glslc
-			PATHS
-			${VULKAN_SDK}/Bin
-			$ENV{VULKAN_SDK}/Bin
-		)
-	elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
-		find_library(Vulkan_LIBRARY
-			NAMES vulkan-1
-			PATHS
-			${VULKAN_SDK}/Lib32
-			${VULKAN_SDK}/Bin32
-			$ENV{VULKAN_SDK}/Lib32
-			$ENV{VULKAN_SDK}/Bin32
-			NO_SYSTEM_ENVIRONMENT_PATH
-		)
-		find_program(SPIRV_COMPILER
-			NAMES glslc
-			PATHS
-			${VULKAN_SDK}/Bin32
-			$ENV{VULKAN_SDK}/Bin32
-		)
-	endif()
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        find_library(Vulkan_LIBRARY
+            NAMES vulkan-1
+            PATHS
+            ${VULKAN_SDK}/Lib
+            ${VULKAN_SDK}/Bin
+            $ENV{VULKAN_SDK}/Lib
+            $ENV{VULKAN_SDK}/Bin
+        )
+        find_program(SPIRV_COMPILER
+            NAMES glslc
+            PATHS
+            ${VULKAN_SDK}/Bin
+            $ENV{VULKAN_SDK}/Bin
+        )
+    elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+        find_library(Vulkan_LIBRARY
+            NAMES vulkan-1
+            PATHS
+            ${VULKAN_SDK}/Lib32
+            ${VULKAN_SDK}/Bin32
+            $ENV{VULKAN_SDK}/Lib32
+            $ENV{VULKAN_SDK}/Bin32
+            NO_SYSTEM_ENVIRONMENT_PATH
+        )
+        find_program(SPIRV_COMPILER
+            NAMES glslc
+            PATHS
+            ${VULKAN_SDK}/Bin32
+            $ENV{VULKAN_SDK}/Bin32
+        )
+    endif()
 else()
-	find_path(Vulkan_INCLUDE_DIR
-		NAMES vulkan/vulkan.h
-		PATHS
-		${VULKAN_SDK}/x86_64/include
-		$ENV{VULKAN_SDK}/x86_64/include
-	)
-	find_library(Vulkan_LIBRARY
-		NAMES vulkan
-		PATHS
-		${VULKAN_SDK}/x86_64/lib
-		$ENV{VULKAN_SDK}/x86_64/lib
-	)
-	find_program(SPIRV_COMPILER
-		NAMES glslc
-		PATHS
-		${VULKAN_SDK}/x86_64/bin
-		$ENV{VULKAN_SDK}/x86_64/bin
-	)
+    find_path(Vulkan_INCLUDE_DIR
+        NAMES vulkan/vulkan.h
+        PATHS
+        ${VULKAN_SDK}/x86_64/include
+        $ENV{VULKAN_SDK}/x86_64/include
+    )
+    find_library(Vulkan_LIBRARY
+        NAMES vulkan
+        PATHS
+        ${VULKAN_SDK}/x86_64/lib
+        $ENV{VULKAN_SDK}/x86_64/lib
+    )
+    find_program(SPIRV_COMPILER
+        NAMES glslc
+        PATHS
+        ${VULKAN_SDK}/x86_64/bin
+        $ENV{VULKAN_SDK}/x86_64/bin
+    )
 endif()
 
 find_file(SPIRV2C NAME SPIRV2C.cmake
-	PATHS ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+    PATHS ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
 
 # Note that the include paths and defines should not have
 # the -I or -D prefix, respectively
 function(add_spirv_embed_library)
-	set(options INCLUDE_DIRECTORIES COMPILE_DEFINITIONS COMPILE_OPTIONS)
-	cmake_parse_arguments(PARSE_ARGV 1 SPIRV "" "" "${options}")
+    set(options INCLUDE_DIRECTORIES COMPILE_DEFINITIONS COMPILE_OPTIONS)
+    cmake_parse_arguments(PARSE_ARGV 1 SPIRV "" "" "${options}")
 
-	set(GLSL_INCLUDE_DIRS "")
-	foreach (inc ${SPIRV_INCLUDE_DIRECTORIES})
-		file(TO_NATIVE_PATH "${inc}" native_path)
-		list(APPEND GLSL_INCLUDE_DIRS "-I${native_path}")
-	endforeach()
+    set(GLSL_INCLUDE_DIRS "")
+    foreach (inc ${SPIRV_INCLUDE_DIRECTORIES})
+        file(TO_NATIVE_PATH "${inc}" native_path)
+        list(APPEND GLSL_INCLUDE_DIRS "-I${native_path}")
+    endforeach()
 
-	set(GLSL_COMPILE_DEFNS "")
-	foreach (def ${SPIRV_COMPILE_DEFINITIONS})
-		list(APPEND GLSL_COMPILE_DEFNS "-D${def}")
-	endforeach()
+    set(GLSL_COMPILE_DEFNS "")
+    foreach (def ${SPIRV_COMPILE_DEFINITIONS})
+        list(APPEND GLSL_COMPILE_DEFNS "-D${def}")
+    endforeach()
 
-	# Compile each GLSL file to embedded SPIRV bytecode
-	set(SPIRV_LIB ${ARGV0})
-	set(SPIRV_BINARIES "")
-	foreach (shader ${SPIRV_UNPARSED_ARGUMENTS})
-		get_filename_component(FNAME ${shader} NAME_WE)
-		set(SPV_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${FNAME}.spv)
-		list(APPEND SPIRV_BINARIES ${SPV_OUTPUT})
+    # Compile each GLSL file to embedded SPIRV bytecode
+    set(SPIRV_LIB ${ARGV0})
+    set(SPIRV_BINARIES "")
+    foreach (shader ${SPIRV_UNPARSED_ARGUMENTS})
+        get_filename_component(FNAME ${shader} NAME_WE)
+        set(SPV_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${FNAME}.spv)
+        list(APPEND SPIRV_BINARIES ${SPV_OUTPUT})
 
         # Determine the dependencies for the shader and track them
         execute_process(
             COMMAND ${SPIRV_COMPILER} ${CMAKE_CURRENT_LIST_DIR}/${shader}
-			${GLSL_INCLUDE_DIRECTORIES} ${GLSL_COMPILE_DEFNS}
+            ${GLSL_INCLUDE_DIRECTORIES} ${GLSL_COMPILE_DEFNS}
             --target-env=vulkan1.1 ${SPIRV_COMPILE_OPTIONS} -MM
             OUTPUT_VARIABLE SPV_DEPS_STRING)
 
@@ -128,29 +128,29 @@ function(add_spirv_embed_library)
         string(REPLACE " " ";" SPV_DEPS_LIST "${SPV_DEPS_STRING}")
         list(REMOVE_AT SPV_DEPS_LIST 0)
 
-		add_custom_command(OUTPUT ${SPV_OUTPUT}
-			COMMAND ${SPIRV_COMPILER} ${CMAKE_CURRENT_LIST_DIR}/${shader}
-			${GLSL_INCLUDE_DIRECTORIES} ${GLSL_COMPILE_DEFNS}
-			--target-env=vulkan1.1 -mfmt=c -o ${SPV_OUTPUT} ${SPIRV_COMPILE_OPTIONS}
+        add_custom_command(OUTPUT ${SPV_OUTPUT}
+            COMMAND ${SPIRV_COMPILER} ${CMAKE_CURRENT_LIST_DIR}/${shader}
+            ${GLSL_INCLUDE_DIRECTORIES} ${GLSL_COMPILE_DEFNS}
+            --target-env=vulkan1.1 -mfmt=c -o ${SPV_OUTPUT} ${SPIRV_COMPILE_OPTIONS}
             DEPENDS ${SPV_DEPS_LIST}
-			COMMENT "Compiling ${CMAKE_CURRENT_LIST_DIR}/${shader} to ${SPV_OUTPUT}")
-	endforeach()
+            COMMENT "Compiling ${CMAKE_CURRENT_LIST_DIR}/${shader} to ${SPV_OUTPUT}")
+    endforeach()
 
-	set(SPIRV_EMBED_FILE "${CMAKE_CURRENT_BINARY_DIR}/${SPIRV_LIB}_embedded_spv.h")
-	add_custom_command(OUTPUT ${SPIRV_EMBED_FILE}
-		COMMAND ${CMAKE_COMMAND} 
-		-DSPIRV_EMBED_FILE=${SPIRV_EMBED_FILE} -DOUTPUT_DIR=${CMAKE_CURRENT_BINARY_DIR}
-		-P ${SPIRV2C}
-		DEPENDS ${SPIRV_BINARIES}
-		COMMENT "Embedding SPIRV bytecode into ${SPIRV_EMBED_FILE}")
+    set(SPIRV_EMBED_FILE "${CMAKE_CURRENT_BINARY_DIR}/${SPIRV_LIB}_embedded_spv.h")
+    add_custom_command(OUTPUT ${SPIRV_EMBED_FILE}
+        COMMAND ${CMAKE_COMMAND} 
+        -DSPIRV_EMBED_FILE=${SPIRV_EMBED_FILE} -DOUTPUT_DIR=${CMAKE_CURRENT_BINARY_DIR}
+        -P ${SPIRV2C}
+        DEPENDS ${SPIRV_BINARIES}
+        COMMENT "Embedding SPIRV bytecode into ${SPIRV_EMBED_FILE}")
 
-	set(SPIRV_CMAKE_CUSTOM_WRAPPER ${SPIRV_LIB}_custom_target)
-	add_custom_target(${SPIRV_CMAKE_CUSTOM_WRAPPER} ALL DEPENDS ${SPIRV_EMBED_FILE})
+    set(SPIRV_CMAKE_CUSTOM_WRAPPER ${SPIRV_LIB}_custom_target)
+    add_custom_target(${SPIRV_CMAKE_CUSTOM_WRAPPER} ALL DEPENDS ${SPIRV_EMBED_FILE})
 
-	add_library(${SPIRV_LIB} INTERFACE)
-	add_dependencies(${SPIRV_LIB} ${SPIRV_CMAKE_CUSTOM_WRAPPER})
-	target_include_directories(${SPIRV_LIB} INTERFACE
-		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>)
+    add_library(${SPIRV_LIB} INTERFACE)
+    add_dependencies(${SPIRV_LIB} ${SPIRV_CMAKE_CUSTOM_WRAPPER})
+    target_include_directories(${SPIRV_LIB} INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>)
 endfunction()
 
 set(Vulkan_LIBRARIES ${Vulkan_LIBRARY})
@@ -158,15 +158,15 @@ set(Vulkan_INCLUDE_DIRS ${Vulkan_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Vulkan
-	DEFAULT_MSG
-	Vulkan_LIBRARY Vulkan_INCLUDE_DIR)
+    DEFAULT_MSG
+    Vulkan_LIBRARY Vulkan_INCLUDE_DIR)
 
 mark_as_advanced(Vulkan_INCLUDE_DIR Vulkan_LIBRARY)
 
 if(Vulkan_FOUND AND NOT TARGET Vulkan::Vulkan)
-	add_library(Vulkan::Vulkan UNKNOWN IMPORTED)
-	set_target_properties(Vulkan::Vulkan PROPERTIES
-		IMPORTED_LOCATION "${Vulkan_LIBRARIES}"
-		INTERFACE_INCLUDE_DIRECTORIES "${Vulkan_INCLUDE_DIRS}")
+    add_library(Vulkan::Vulkan UNKNOWN IMPORTED)
+    set_target_properties(Vulkan::Vulkan PROPERTIES
+        IMPORTED_LOCATION "${Vulkan_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Vulkan_INCLUDE_DIRS}")
 endif()
 
